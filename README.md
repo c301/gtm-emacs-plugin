84dc1a1fa78d856ac6149be11801c56289724560
Anton Sivolapov <anton.sivolapov@gmail.com>
2018-11-05 16:32:13 -0800

    address linter issues

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Plugin for the Emacs editor to be used with the [Git Time Metric](https://github.com/git-time-metric/gtm) platform.
 
 ## Usage
-`M-x gtm-git-time-metric-record`
+`M-x gtm-emacs-plugin-record`
 
 To automatically record time after saving:
 
@@ -10,15 +10,15 @@ To automatically record time after saving:
 
 ```
 (eval-after-load 'js-mode
-	   '(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-git-time-metric-record))))
+	   '(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-emacs-plugin-record))))
 
 (eval-after-load 'js2-mode
-	   '(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-git-time-metric-record))))
+	   '(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-emacs-plugin-record))))
 ```
 
 or track all your files
 
 ```
-(add-hook 'after-save-hook 'gtm-git-time-metric-record)
+(add-hook 'after-save-hook 'gtm-emacs-plugin-record)
 ```
 
--- gtm-emacs-plugin.el
+++ gtm-emacs-plugin.el
@@ -12,56 +12,56 @@
 
 ;;; Commentary:
 ;;
-;; This file provides `gtm-git-time-metric-record', which records time with gtm. Essentially it calls `gtm record [options] <file_name>'.
+;; This file provides `gtm-emacs-plugin-record', which records time with gtm. Essentially it calls `gtm record [options] <file_name>'.
 ;;
 ;; Usage:
-;;     M-x gtm-git-time-metric-record
+;;     M-x gtm-emacs-plugin-record
 ;;
 ;;     To automatically record time after saving:
 ;;     (Choose depending on your favorite mode.)
 ;;
 ;; (eval-after-load 'js-mode
-;; 	'(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-git-time-metric-record))))
+;; 	'(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-emacs-plugin-record))))
 ;;
 ;; (eval-after-load 'js2-mode
-;; 	'(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-git-time-metric-record))))
+;; 	'(add-hook 'js-mode-hook (lambda () (add-hook 'after-save-hook 'gtm-emacs-plugin-record))))
 ;;
 ;;     Or track all your files:
 ;;
-;; (add-hook 'after-save-hook 'gtm-git-time-metric-record)
+;; (add-hook 'after-save-hook 'gtm-emacs-plugin-record)
 
 ;;; Code:
-(defgroup gtm-git-time-metric-record nil
-  "Record to gtm with ‘gtm-git-time-metric-record’."
-  :link '(function-link gtm-git-time-metric-record)
+(defgroup gtm-emacs-plugin-record nil
+  "Record to gtm with ‘gtm-emacs-plugin-record’."
+  :link '(function-link gtm-emacs-plugin-record)
   :tag "Git Time Metric (gtm) Record"
   :group 'tools)
 
-(defcustom gtm-git-time-metric-record-executable "gtm"
+(defcustom gtm-emacs-plugin-executable "gtm"
   "Set gtm executable to use."
   :tag "Git Time Metric (gtm) Executable"
   :type 'string)
 
-(defcustom gtm-git-time-metric-record-options nil
+(defcustom gtm-emacs-plugin-executable-options nil
   "Additional options to pass to gtm (e.g. “-status=false”)."
   :tag "Git Time Metric (gtm) Options"
   :type '(repeat string))
 
 ;;;###autoload
-(defun gtm-git-time-metric-record()
+(defun gtm-emacs-plugin-record()
   "Record to gtm (git time metric), ie call ‘gtm record <file_name>’."
   (interactive)
   (unless buffer-file-name
     (error "Git Time Metric requires a file-visiting buffer"))
 
-  (let ((gtm (executable-find gtm-git-time-metric-record-executable))
+  (let ((gtm (executable-find gtm-emacs-plugin-executable))
         (options (append (list "record")
-                         gtm-git-time-metric-record-options
+                         gtm-emacs-plugin-executable-options
                          (list buffer-file-name))))
     (unless gtm
-      (error "Executable ‘%s’ not found" gtm-git-time-metric-record-executable))
+      (error "Executable ‘%s’ not found" gtm-emacs-plugin-executable))
     (apply 'call-process gtm nil "*gtm Errors*" nil options)))
 
-(provide 'gtm-git-time-metric-record)
+(provide 'gtm-emacs-plugin)
 
 ;;; gtm-emacs-plugin.el ends here
